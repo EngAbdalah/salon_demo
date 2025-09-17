5aa45df84b3fe47851e18eacae0a31d2277fe67c
abdulrahman <abdulrahman@abadc.com.sa>
2025-09-17 14:06:18 +0300

    salon_demo/

---
+++ salon_demo/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Salon Demo',
+    'version': '17.0.1.0.0',
+    'summary': 'Demo: Default calendar view and quick create for sale orders',
+    'category': 'Sales',
+    'website': '',
+    'depends': ['sale'],
+    'data': [
+        'views/sale_order_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
